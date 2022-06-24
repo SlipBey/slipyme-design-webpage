804eef4007510055ed946ff8c93fa3f1f450bf38
SlipBey <52493196+SlipBey@users.noreply.github.com>
2022-06-24 11:30:10 +0300

    Update teams.ts

--- libs/config/teams.ts
+++ libs/config/teams.ts
@@ -3,11 +3,21 @@ import ahmeteren from "@assets/ahmet-eren.svg";
 export const Teams = [
 	{
 		name: "Ahmed-i Fünun",
-		job: "Tasarımcı/AI",
+		job: "Tasarımcı",
 		slipyme: "ahmedi-funun",
 		social: {
 			discord: "Belki bir gün#9002",
 			instagram: "ahmet_eren_259",
+		},
+		image: ahmeteren,
+	},
+	{
+		name: "004_Macit",
+		job: "Tasarımcı",
+		slipyme: "004-macit",
+		social: {
+			discord: "004_Macit#2087",
+			instagram: "004_macit",
 		},
 		image: ahmeteren,
 	},
